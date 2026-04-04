3bf62750aceb66a346d3f49aa191d141594649ad
unknown <sojibjubayer@gmail.com>
2026-04-04 21:46:52 +0300

    zenvisa to visa slide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "ZenVisas | Automated Visa Platform",
+  title: "Visa Slide | Automated Visa Platform",
   description: "Simple, automated, and secure visa assistance.",
 };
 
